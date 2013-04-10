dc47a997b8645c40ba9e9f0b64f3160a05dd7a89
guillaume <guillaume@smartere.com>
2013-04-10 17:52:29 +0200

    changes on nav + humans

--- js/nav.js
+++ js/nav.js
@@ -96,7 +96,159 @@ $(document).ready(function() {
 		    slideshow: false,
 		    sync: "#carousel"
 		  });
-		  		  
+		  	
+		 /* 
+		  var lastScrollTop = 0;
+		  $(window).scroll(function(event){
+		     var st = $(this).scrollTop();
+		     if (st > lastScrollTop){
+		         //scroll down
+		    	 $('#intro').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						   if (visiblePartY == 'bottom') {
+							  // bottom part of element is visible
+							  $('#lien1').addClass("btn-gris");
+							  $('#lien1').removeClass("active");
+							  $('#lien2').addClass("active");
+							  $('#lien2').removeClass("btn-gris");
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien2').addClass("btn-gris");
+							  $('#lien2').removeClass("active");
+							  $('#lien1').addClass("active");
+							  $('#lien1').removeClass("btn-gris");
+						  }
+					  }
+				  });
+		    	 $('#slider').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'bottom') {
+							  // top part of element is visible
+							  $('#lien2').addClass("active");
+							  $('#lien2').removeClass("btn-gris");
+							  $('#lien1').addClass("btn-gris");
+							  $('#lien1').removeClass("active");
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien2').addClass("active");
+							  $('#lien2').removeClass("btn-gris");
+							  $('#lien1').addClass("btn-gris");
+							  $('#lien1').removeClass("active");
+						  }
+					  }
+				  });
+		    	 $('.skillbar2').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'top') {
+							  // top part of element is visible
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien3').addClass("active");
+							  $('#lien3').removeClass("btn-gris");
+							  $('#lien2').addClass("btn-gris");
+							  $('#lien2').removeClass("active");
+						  }
+					  }
+				  });
+		    	 $('#form').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'top') {
+							  // top part of element is visible
+							  $('#lien4').addClass("active");
+							  $('#lien4').removeClass("btn-gris");
+							  $('#lien3').addClass("btn-gris");
+							  $('#lien3').removeClass("active");
+						  }
+						  else {
+							  // whole part of element is visible
+						  }
+					  }
+				  });
+		     } 
+		     
+		     
+		     
+		     
+		     else {
+		    	 // scroll up
+		    	 $('#intro').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						   if (visiblePartY == 'bottom') {
+							  // bottom part of element is visible
+							  $('#lien1').addClass("btn-gris");
+							  $('#lien1').removeClass("active");
+							  $('#lien2').addClass("active");
+							  $('#lien2').removeClass("btn-gris");
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien2').addClass("btn-gris");
+							  $('#lien2').removeClass("active");
+							  $('#lien1').addClass("active");
+							  $('#lien1').removeClass("btn-gris");
+						  }
+					  }
+				  });
+		    	 $('#slider').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'bottom') {
+							  // top part of element is visible
+							  $('#lien2').addClass("active");
+							  $('#lien2').removeClass("btn-gris");
+							  $('#lien3').addClass("btn-gris");
+							  $('#lien3').removeClass("active");
+						  }
+						  else {
+							  // whole part of element is visible
+						  }
+					  }
+				  });
+		    	 $('.skillbar2').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'top') {
+							  // top part of element is visible
+							  $('#lien3').addClass("active");
+							  $('#lien3').removeClass("btn-gris");
+							  $('#lien4').addClass("btn-gris");
+							  $('#lien4').removeClass("active");
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien3').addClass("active");
+							  $('#lien3').removeClass("btn-gris");
+							  $('#lien4').addClass("btn-gris");
+							  $('#lien4').removeClass("active");
+						  }
+					  }
+				  });
+		    	 $('#form').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
+					  if (isInView) {
+						  // element is now visible in the viewport
+						  if (visiblePartY == 'top') {
+							  // top part of element is visible
+						  }
+						  else {
+							  // whole part of element is visible
+							  $('#lien4').addClass("active");
+							  $('#lien4').removeClass("btn-gris");
+						  }
+					  }
+				  });
+		     }
+		     lastScrollTop = st;
+		  });
+		  */
+		  
 		  $('.skillbar2').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
 			  if (isInView) {
 				  // element is now visible in the viewport
